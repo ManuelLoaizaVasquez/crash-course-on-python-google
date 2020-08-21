57f401d577df3afca3442d3718837edfae4c985b
ManuelLoaizaVasquez <manulova999@gmail.com>
2020-08-21 13:01:40 -0500

    Leer, escribir y operar en python.

---
+++ week1_hello_python/hello_world.py
@@ -0,0 +1,34 @@
+print("Hello, World!")
+
+name = "Manuel"
+print("Hello " + name)
+name = "Alejandro"
+print("Hello " + name)
+
+print(4 + 5)
+print(9 * 7)
+print(-1 / 4)
+print(1 / 3)
+print(2 ** 10)
+print((((1 + 2) * 3) / 4) ** 5)
+
+ratio = (1 + 5 ** (1 / 2)) / 2
+print(ratio)
+
+print( "Automating with Python is fun!")
+
+color = "Yellow"
+thing = "sunshine"
+print(color + " is the color of " + thing)
+
+# s / week = s / day * day / week
+seconds_in_week = 86400 * 7
+print(seconds_in_week)
+
+ways = 26 ** 6
+print(ways)
+
+disk_size = 16*1024*1024*1024
+sector_size = 512
+sector_amount = disk_size // sector_size
+print(sector_amount)
