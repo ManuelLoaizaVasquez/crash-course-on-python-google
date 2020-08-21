7d679bda2428f884e2aa1a542f374b0ea271074f
ManuelLoaizaVasquez <manulova999@gmail.com>
2020-08-21 13:54:24 -0500

    variables, asignacion, expresion, conversion implicita

---
+++ week2_basic_python_syntax/variables.py
@@ -0,0 +1,42 @@
+# variables : nombres que le damos a ciertos valores en nuestros programas
+# asignacion : procesor de guardar un valor dentro de una variable
+length = 10
+width = 2
+# expresion: combinacion de numeros, simbolos y otras variables para producir un resultado al ser evluados
+area = length * width
+print(area)
+
+# implicit conversion: el interprete automaticamente convierte un tipo de dato en otro
+print(7 + 8.5)
+base = 6
+height = 3
+area = base * height / 2
+print("The area of the triangle is " + str(area))
+
+total = 2048 + 4357 + 97658 + 125 + 8
+files = 5
+average = total / files
+print("The average size is: " + str(average))
+
+bill = 47.28
+tip = bill * 0.15
+total = bill + tip
+share = total / 2
+print("Each person needs to pay: " + str(share))
+
+numerator = 10
+denominator = 10
+result = numerator // denominator
+print(result)
+
+word1 = "How"
+word2 = "do"
+word3 = "you"
+word4 = "like"
+word5 = "Python"
+word6 = "so"
+word7 = "far?"
+sentence = word1 + " " + word2 + " " + word3 + " " + word4 + " " + word5 + " " + word6 + " " + word7
+print(sentence)
+
+print("2 + 2 = " + str(2 + 2))
